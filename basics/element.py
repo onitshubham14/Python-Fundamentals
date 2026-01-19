f211462ce28023dc7bbdfa0d89ee1d82dee73868
Kunal <kunalburnwal7@gmail.com>
2026-01-19 15:59:37 +0530

    Added basic Python elements and folder structure

---
+++ basics/element.py
@@ -0,0 +1,24 @@
+# This file demonstrates basic Python statements.
+
+# Assignment statement
+x = 5       # variable x stores the value 5.
+print(x)    # retrieve and display the value of variable x.
+
+# Expression and reassignment
+x = x + 5   # takes the current value of x, adds 5 in it, store back its final value in x.
+print(x)    # print the final value of x **10**. 
+
+#operators
+y =  -4
+sum_value = x + y  # add value of x and y, store the result in sum_value
+print(sum_value)   # display the calculated sum from variable.
+
+# print() is a built-in function
+print("Learning Python fundamentals")  
+
+#things to know : 
+# = is assignment, not math equality.
+# python Read right side first, Then store result in left variable.
+# print is a function Values inside () are arguments.
+# if syntax is wrong, Python raises a SyntaxError
+# example : x = x +
---
+++ basics/intro.py
@@ -0,0 +1,8 @@
+#this is my very first code in python, my learning is being done through coursera **Python For Everybody**  course by DR.Chuck
+# this code prints the very simple output using print function in python.  **Hello world!**
+
+#FunFact: Python isn’t named after a snake. It’s named after Monty Python 
+# — which explains why coding in Python is fun until you see an error. =^.^=
+
+
+print('Hello world!')
