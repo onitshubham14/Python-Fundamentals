b43c3aa9f48dac3dd4809870131796f8f9ca77f7
shubham <shubhambarnwal046@gmail.com>
2026-01-21 15:38:17 +0530

    Done some hands-on exercises.

---
+++ basics/gross_pay.py
@@ -0,0 +1,19 @@
+# Assignment: Gross Pay Calculator
+
+# Calculate gross pay using user input
+# Inputs: hours worked, rate per hour
+# Output: total pay after calculation
+
+hrs = input("Enter Hours:")
+hr_rate = input("enter rate per hour:")
+
+hrs = float(hrs)
+hr_rate = float(hr_rate)
+
+grosspay = hrs*hr_rate 
+print("Pay:", grosspay)
+
+# Example test case:
+# Hours: 35
+# Rate: 2.75
+# Expected Output: Gross Pay = 96.25
---
+++ basics/ipo_elevator.py
@@ -0,0 +1,11 @@
+# This folder is demonstrate Input, Processing, and Output (IPO)
+
+# Convert European elevator floor to US floor
+
+inp = input("European floor? ") # Input : get the european floor number from the user.
+
+usf = int(inp) + 1 # Process :  convert input to integer and add 1
+
+print("US floor:", usf) # Output : display the US floor number
+
+# Fact: Europe starts floors at 0, US starts at 1.
